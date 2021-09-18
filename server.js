49b6c037178fc1dea412321285648e95b201761f
Monsif96 <monsif.ghazaui.tab@gmail.com>
2021-09-18 12:56:14 +0100

    commit

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
+//const bodyParser = require('body-parser');
+//const bcrypt = require('bcrypt-nodejs');
+//const cors = require('cors');
+const knex = require('knex');
+
+const profile = require('./controllers/profile');
+
+
+//Connect to DataBase--------------------------------------------------------------------------------------------------------------
+const Db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+});
 
 const app = express();
 
 //----Home--------------------------------------------------------------------------------------------------------------
 app.get('/', (req, res) => {res.json('its Working !!!!');})
 
+//----Profile--------------------------------------------------------------------------------------------------------------
+app.get('/profile/:id', (req, res) => { profile.handleprofile(req, res, Db)} )
 
 
 
